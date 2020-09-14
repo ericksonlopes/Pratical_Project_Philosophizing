68f633880934bc052cef4b580085967a0f0524c1
EricksonLopes <ofc.erickson@gmail.com>
2020-09-14 17:19:41 -0300

    fix acl

--- philosophizing/settings.py
+++ philosophizing/settings.py
@@ -158,6 +158,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
